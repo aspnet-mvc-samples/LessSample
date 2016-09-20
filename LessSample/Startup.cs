52e4ea3a8a7833cf455c7703ab1c250397b671fe
kobake <kobake@users.sourceforge.net>
2016-09-21 04:59:05 +0900

    Add project files.

---
+++ LessSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LessSample.Startup))]
+namespace LessSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
