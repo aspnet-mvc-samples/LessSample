9e4b6321264dd16d0e002ad75f7691a556cfc090
kobake <kobake@users.sourceforge.net>
2016-09-21 05:51:32 +0900

    LESS 組み込み

--- LessSample/App_Start/BundleConfig.cs
+++ LessSample/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace LessSample
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/app.less"));
         }
     }
 }
